1e7de7a039d869f8f10cd1a1f81ecbfa70490b2c
Ankit rahi <120669540+Ankit9179@users.noreply.github.com>
2024-02-24 09:42:27 +0530

    cors setup

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
+//cors configuring
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+//
+app.use(express.json()); // parse the json data in javascript from frontedn
+app.use(express.urlencoded({ extended: true }));
 
 export default app;
