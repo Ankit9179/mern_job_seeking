8e0c77e9f9dcb1e8e5028736d43d73490187ce37
Ankit rahi <120669540+Ankit9179@users.noreply.github.com>
2024-03-07 23:19:06 +0530

    design for footer

--- frontend/src/components/Layout/Footer.jsx
+++ frontend/src/components/Layout/Footer.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import './Footer.css'
+import { Link } from 'react-router-dom'
+import { FaGithub } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa6";
+import { FaInstagramSquare } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <>
+            <div className="fcontainer">
+                <div className="footer">
+                    <div className="left">
+                        <ul>
+                            <li>home</li>
+                            <li>about</li>
+                            <li>contact</li>
+                            <li>doc.</li>
+                        </ul>
+                    </div>
+                    <div className="right">
+                        <Link to='https://www.linkedin.com/in/ankit-rahi-563752258/'>
+                            <FaLinkedin />
+                        </Link>
+                        <Link to="https://github.com/Ankit9179">
+                            <FaGithub />
+                        </Link>
+                        <Link to="https://www.instagram.com/ak_vampire12/?next=%2F">
+                            <FaInstagramSquare />
+                        </Link>
+                    </div>
+                </div>
+                <p>Copyright &copy; 2024 created by ANKIT</p>
+            </div>
+        </>
     )
 }
 
