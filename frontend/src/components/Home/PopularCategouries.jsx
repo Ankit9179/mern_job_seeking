9b2d0dd811410f12b13cda8d5937708f24f21e0d
Ankit rahi <120669540+Ankit9179@users.noreply.github.com>
2024-03-28 21:04:06 +0530

    code for some components

--- frontend/src/components/Home/PopularCategouries.jsx
+++ frontend/src/components/Home/PopularCategouries.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import './Popular.css'
 
 const PopularCategouries = () => {
+    const num = [1, 2, 3, 4, 5, 6];
     return (
-        <div>PopularCategouries</div>
+        <>
+            <div className='popular-card-contienr'>
+                {
+                    num.map((i) => {
+                        return (
+                            <div class="popular-card">
+                                <div class="popular-card-body">
+                                    <h5 class="popular-card-title">Card title{i}</h5>
+                                    <p class="popular-card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <a href="#" class="popular-btn">Go somewhere</a>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </>
     )
 }
 
--- frontend/src/components/Home/PopularCompanies.jsx
+++ frontend/src/components/Home/PopularCompanies.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import './PopularCompany.css'
 
 const PopularCompanies = () => {
     return (
-        <div>PopularCompanies</div>
+        <>
+            <div className="popularConpanies-container">
+                <div className="popularConpanies-container-content">
+                    <div className="popularConpanies-container-card">
+                        <h1>MicroSoft</h1>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet nulla laboriosam modi eos veniam sapiente exercitationem, odit quidem t, repellendus.</p>
+                    </div>
+                </div>
+                <div className="popularConpanies-container-content">
+                    <div className="popularConpanies-container-card">
+                        <h1>Apple</h1>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet nulla laboriosam modi eos veniam sapiente exercitationem, odit quidem t, repellendus.</p>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
